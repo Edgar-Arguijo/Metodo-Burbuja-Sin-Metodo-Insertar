5c737c32be8fa3b7bbfd656022123609ccef7619
Arguijo Vazquez <arguijovazquez@hotmail.com>
2020-11-30 15:51:27 -0600

    Agregar archivos de proyecto.

---
+++ Metod Burbuja Sencillo/Burbuja.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metod_Burbuja_Sencillo
+{
+    class Burbuja
+    {
+        // Metodo para Intercambiar posiciones
+        public void Intercambio(int[] valores)
+        {
+            //Variable auxiliar
+            int aux;
+
+            //Metodo burbuja
+            for (int i = 1; i < valores.Length; i++)
+                for (int j = valores.Length - 1; j >= i; j--)
+                {
+                    if (valores[j] < valores[j - 1])
+                    {
+                        aux = valores[j - 1];
+                        valores[j - 1] = valores[j];
+                        valores[j] = aux;
+                    }
+                }
+        }
+        //Metodo para Mostrar
+        public void Mostrar(int[] valores)
+        {
+            //arreglo para mostrar valores
+            for (int i = 0; i <= 7; i++)
+            {
+                //Muestra los valores del arreglo
+                Console.WriteLine("Elemento[ " + i + " ]" + " = " + valores[i]);
+            }
+        }
+
+    }
+}
---
+++ Metod Burbuja Sencillo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metod_Burbuja_Sencillo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------------Metodo Burbuja------------------------------------");
+            Console.WriteLine();
+
+            //Declaracion de objeto
+            Burbuja Obj1 = new Burbuja();
+
+            //Arreglo ya con valores
+            int[] valores = { 4, 8, 90, 23, 1, 67, 54, 16 };
+
+            Console.Write("---------------------------------- Elementos aun no ordenados: --------------------------");
+            Console.WriteLine();
+            Obj1.Mostrar(valores);
+
+            Console.Write("----------------------------- Elementos Ordenados de Menor a Mayor: ----------------------");
+            Console.WriteLine();
+            Obj1.Intercambio(valores);
+            Obj1.Mostrar(valores);
+            Console.ReadKey();
+        }
+    }   
+}
+
